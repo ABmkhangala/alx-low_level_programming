c6e94f5487449cbe698a807dea54d3c46a9fcee5
ABmkhangala <Abongilemkhangala@outlook.com>
2023-02-18 05:09:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- 
  * main - A c program that prints a line to the standard error
- *Return 1 (Success)
+ * Return: 1 (Success)
  */
-int main(void)
+int main(void) /*code comes here*/
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
-	return (1)
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
